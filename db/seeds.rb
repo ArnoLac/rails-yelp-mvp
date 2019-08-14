92b7af76a0998c3cbc03a71e85237c2f270fc0bd
Arnaud Lacombe <arnaud.lacombe@whisp.tech>
2019-08-14 14:31:31 +0200

    app fonctionnelle hors reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+(1..5).each { Restaurant.create(name: Faker::Restaurant.name, phone_number: '0134621233', address: Faker::Address.full_address, category: 'chinese') }
